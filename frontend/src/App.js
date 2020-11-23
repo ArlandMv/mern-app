7030ada19f0a104e168422d4600c7b8f7996801a
Arland Michelena <arlandmvillegas@gmail.com>
2020-11-23 19:39:42 -0300

    added header and footer components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,19 @@
 //import logo from './logo.svg';
+import Header from './components/Header'  
+import Footer from './components/Footer'
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
+      <Header />
+      <body>
         <h1>Bienvenido a TuApp</h1>
         <p>
           Editame and save to reload.
         </p>
-      </header>
+      </body>
+      <Footer />
     </div>
   );
 }
