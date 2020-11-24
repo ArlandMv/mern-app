e84789d60d67151b55a11f63eedd72e93facbc3c
Arland Michelena <arlandmvillegas@gmail.com>
2020-11-24 16:30:14 -0300

    added eslint and setted node lts

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <header/>
+      <p>Edit <code>src/App.js</code> and save to reload.</p>
+    </>
+  );
+}
+
+export default App;
